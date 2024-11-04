70e275151ea06b65fd699c95eb893884d53c79ad
Debasmitasinha <150664862+Debasmitasinha@users.noreply.github.com>
2024-11-04 12:55:22 +0530

    Add files via upload

--- src/content_option.js
+++ src/content_option.js
@@ -1,7 +1,7 @@
-const logotext = "Devasmii";
+const logotext = "Debasmita";
 const meta = {
     title: "Debasmita Sinha",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    description: "I'm always a learner.",
 };
 
 const introdata = {
@@ -9,138 +9,100 @@ const introdata = {
     animated: {
         first: "I love coding",
         second: "I love dancing",
-        third: "I love Cybersecurity",
+        third: "I'm into Cybersecurity",
     },
-    description: "Chiki ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://krishnasinhablog.wordpress.com/wp-content/uploads/2021/01/img_20210110_232423.jpg",
+    description: "A soulful dancer, model and achiever.",
+    your_img_url: "https://scontent-bom2-1.xx.fbcdn.net/v/t1.6435-9/184652807_916653609176434_4203298073526394423_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=K3pIoDInIygQ7kNvgFRI15p&_nc_zt=23&_nc_ht=scontent-bom2-1.xx&_nc_gid=AISaB94Mowocs40ZkT72ygm&oh=00_AYBx_eb-C5PUhZIRVQADqUwV5lVPuj-f6GzTb4SP-kpWgg&oe=67438B44",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "A bit about my self",
+    aboutme: "Hello ! I'm Debasmita , a passionate classical dance practitioner and model from India. I have been practicing classical dance since childhood and have participated in numerous national and international events. My journey in the world of dance has been enriching and has taught me the importance of discipline, grace, and perseverance. Alongside my dance career, I also pursue modeling, bringing the same dedication and passion to every project I undertake.Apart from that I'm a Computer Technology student at Assam down town University. Welcome to my portfolio, where you can explore my achievements, gallery, and more about my journey.",
 };
 const worktimeline = [{
-        jobtitle: " MISS TEEN TALENTED INDIA UNIVERSE '21",
-        where: "YAdfi",
+        jobtitle: "Class Representative",
+        where: "Assam down town University",
+        date: "September 2023-October 2024",
+    },
+    {
+        jobtitle: "P&B National Level Dance Champ 2022 ",
+        where: "Uttar Pradesh",
+        date: "2022",
+    },
+    {
+        jobtitle: "Miss Northeast Beauty Queen 2020",
+        where: "Silchar",
         date: "2020",
-    },
-    {
-        jobtitle: "P&B National Level Dance Champ '22 ",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
     },
 ];
 
 const skills = [{
         name: "Python",
+        value: 30,
+    },
+    {
+        name: "C",
         value: 90,
     },
     {
-        name: "Djano",
-        value: 85,
+        name: "C++",
+        value: 70,
     },
     {
         name: "Javascript",
-        value: 80,
+        value: 40,
     },
     {
         name: "React",
         value: 60,
     },
     {
-        name: "Jquery",
-        value: 85,
+        name: "HTML",
+        value: 95,
     },
 ];
 
 const services = [{
         title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "Design and optimize user interfaces and experiences to ensure visually appealing, intuitive, and user-centered digital products.",
     },
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Security Developer",
+        description: "Design, implement and maintain security measures to protect software and systems from vulnerabilities, ensuring data integrity, confidentiality, and regulatory compliance.",
     },
     {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Frontend Developer",
+        description: "Build and optimize user-facing elements of web applications, ensuring responsive, interactive, and visually appealing interfaces through code and design.",
     },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent-bom2-2.xx.fbcdn.net/v/t39.30808-6/358149491_731433148987841_3926955074328499823_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=MfZURqLuf2QQ7kNvgGrg0lc&_nc_zt=23&_nc_ht=scontent-bom2-2.xx&_nc_gid=ATNhTTRk86UcYjECodrz4JS&oh=00_AYA5RPuYs9K0WKsfDEZRChu7jsMcSEJybgv9rZbKcaCkmQ&oe=6721CCFD",
+        description: "Global Talent Icon Award 2023",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent-bom1-2.xx.fbcdn.net/v/t39.30808-6/436770716_912353614229126_1829792521949545126_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=7uhgSOZz27sQ7kNvgHGucU6&_nc_zt=23&_nc_ht=scontent-bom1-2.xx&_nc_gid=AU-TH-tEpToOcSQqxEr1XuD&oh=00_AYDvLOBWYVrxf6qNiGn5S4z1suv86PfvEoB9_RBZxIzsUg&oe=6721EADA",
+        description: "The Rising Star Award 2023",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent-bom1-2.xx.fbcdn.net/v/t39.30808-6/345471947_656534369817655_8327253277789584100_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=qSwSybUt9-AQ7kNvgGba15r&_nc_zt=23&_nc_ht=scontent-bom1-2.xx&_nc_gid=AW-3jClSBAzHPU_1dycmsWY&oh=00_AYAF1zODKvWNOHXwkfUw2snQ3pfT2pBxftKQX6gJy_VsHg&oe=6721F0B0",
+        description: "Best Model of the Year Award 2023.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent-bom1-2.xx.fbcdn.net/v/t39.30808-6/358133799_731433225654500_4844565920782725878_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=lQwrsjIItu0Q7kNvgGtfoiH&_nc_zt=23&_nc_ht=scontent-bom1-2.xx&_nc_gid=ARxTjLgAgyGHnLdE9LOojCV&oh=00_AYDB3n3xL4tSzwycnZKEunCLuYRiFwnpbsCPR7Gpd48qkQ&oe=6721C2F8",
+        description: "Indian Achievers Award 2023",
         link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+    }
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_EMAIL: "debasmitasinha071@gmail.com",
+    YOUR_PHONE: "8837483396",
+    description: "Hello ! I'm Debasmita , a passionate classical dance practitioner and model from India. I have been practicing classical dance since childhood and have participated in numerous national and international events. My journey in the world of dance has been enriching and has taught me the importance of discipline, grace, and perseverance. Alongside my dance career, I also pursue modeling, bringing the same dedication and passion to every project I undertake. Welcome to my portfolio, where you can explore my achievements, gallery, and more about my journey.",
+
     YOUR_SERVICE_ID: "service_id",
     YOUR_TEMPLATE_ID: "template_id",
     YOUR_USER_ID: "user_id",
@@ -148,7 +110,7 @@ const contactConfig = {
 
 const socialprofils = {
     github: "https://github.com/Debasmitasinha",
-    facebook: "https://facebook.com",
+    facebook: "https://www.facebook.com/profile.php?id=100063634873812&mibextid=ZbWKwL",
     linkedin: "https://www.linkedin.com/in/debasmita-sinha-b8b414292/",
     twitter: "https://x.com/_debasmitasinha",
 };
